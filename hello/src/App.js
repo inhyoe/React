3a4b08498806723b8e0b5e225d44a767b2718b32
ingyoe <melon7256@naver.com>
2022-05-06 20:32:44 +0900

    비구조화 할당

---
+++ hello/src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Hello from "./components/Hello";
+
+function App() {
+  return <Hello props="react" color="skyblue"></Hello>;
+}
+
+export default App;
--- hello/src/components/Hello.jsx
+++ hello/src/components/Hello.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
-function Hello(props){
-    return <div>Hello {props.name}</div>;
+function Hello({ props, color }){
+    return <div style={{ color }}>Hello {props}</div>;
 }
 
 export default Hello;
+
+// import React from "react";
+
+// function Hello({ name, color }) {
+//   return <div style={{ color }}>Hello {name}</div>;
+// }
+// export default Hello;
