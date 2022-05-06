bb0339f9b5b10d62fe148540e213e5fa2cc6d702
ingyoe <melon7256@naver.com>
2022-05-06 20:36:30 +0900

    DefaultProps

--- hello/src/App.js
+++ hello/src/App.js
@@ -2,7 +2,13 @@ import React from "react";
 import Hello from "./components/Hello";
 
 function App() {
-  return <Hello props="react" color="skyblue"></Hello>;
+    return (
+    <>
+        <Hello props="react" color="skyblue"></Hello>
+        <Hello></Hello>
+    </>
+    );
+    
 }
 
 export default App;
--- hello/src/components/Hello.jsx
+++ hello/src/components/Hello.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 
 function Hello({ props, color }){
+    Hello.defaultProps ={
+        props : "이름없음",
+        color : "red"
+    }; // jsx에서 default값을 줄 수 있음.
     return <div style={{ color }}>Hello {props}</div>;
 }
 
